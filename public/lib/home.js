0670dd0700cd4ec8eb65164f27a8d697ab38b3f4
Steliospne <63586679+Steliospne@users.noreply.github.com>
2024-10-15 12:38:54 +0200

    "Implements the characterAPI to dynamically display data on the home page"

--- public/lib/home.js
+++ public/lib/home.js
@@ -1,16 +1,32 @@
 import { getImage } from "./scripts.js";
+import * as characterAPI from "./data.js";
 
 const heroContainer = document.querySelector(".hero");
+const nameContainer = document.querySelector(".name");
+const quoteContainer = document.querySelector(".quote > i");
 const img = document.createElement("img");
 
 heroContainer?.append(img);
 
-async function test() {
-  img.src = await getImage("/Deva_Path.webp", "image/webp");
+const charId = 0;
+
+try {
+  img.src = await getImage(characterAPI.getCharacterImg(charId), "image/webp");
   document.body.style.backgroundImage = `url(${await getImage(
     "/akatsuki_logo.jpeg",
     "image/jpeg"
   )})`;
+} catch (error) {
+  throw new error();
 }
 
-test();
+const randomQuote = Math.floor(
+  Math.random() * characterAPI.getQuotes(charId).length
+);
+
+if (nameContainer !== null) {
+  nameContainer.textContent = characterAPI.getName(charId);
+}
+if (quoteContainer !== null) {
+  quoteContainer.textContent = characterAPI.getQuote(charId, randomQuote);
+}
