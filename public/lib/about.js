c2d34c14f13846b52060626e3f292f14779568c1
Steliospne <63586679+Steliospne@users.noreply.github.com>
2024-10-16 21:33:46 +0200

    Implements the about page by getting the character's backstory and mapping it to paragraphs for better visibility.

---
+++ public/lib/about.js
@@ -0,0 +1,46 @@
+import * as characterAPI from "./data.js";
+import { getImage } from "./scripts.js";
+
+const title = document.querySelector(".title");
+const backstoryContainer = document.querySelector(".backstory");
+const wikiLink = document.querySelector(".wiki");
+
+const charId = document.URL.split("?")[1];
+
+const root = document.querySelector(":root");
+const colorTheme = charId === "0" ? "crimson" : "sandybrown";
+
+root.style.setProperty("--color-theme", colorTheme);
+console.log(charId);
+const links = Array.from(document.getElementsByName("headerLink"));
+links.forEach((link) => {
+  const url = link.getAttribute("href") + "?" + charId;
+  link.setAttribute("href", url);
+});
+
+try {
+  document.body.style.backgroundImage = `url(${await getImage(
+    characterAPI.getAffilLogo(charId),
+    "image/jpeg"
+  )})`;
+} catch (error) {
+  throw error;
+}
+
+if (title !== null) {
+  title.textContent = `${characterAPI.getName(charId)}'s Backstory`;
+}
+
+if (backstoryContainer !== null) {
+  const paragraphs = characterAPI.getBackstory(charId).split("\n");
+  paragraphs.map((paragraph) => {
+    const paragraphEle = document.createElement("p");
+    paragraphEle.textContent = paragraph;
+    backstoryContainer.append(paragraphEle);
+  });
+}
+
+if (wikiLink !== null) {
+  wikiLink.setAttribute("href", characterAPI.getWikiURL(charId));
+  wikiLink.textContent += " " + characterAPI.getWikiURL(charId);
+}
